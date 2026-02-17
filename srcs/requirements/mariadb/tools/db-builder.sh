1f92f08ffb4b83f16190f5754a86faea456d34a3
bn-khalil <benkhaliltouda3201@gmail.com>
2026-02-17 09:04:58 -0500

    feat: connect mariadb wordpress

--- srcs/requirements/mariadb/tools/db-builder.sh
+++ srcs/requirements/mariadb/tools/db-builder.sh
@@ -4,12 +4,30 @@ service mariadb start
 
 sleep 5
 
-mariadb -u root << EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-ALTER USER 'root'@'localhost' IDENTIFIED BY $ROOT_PASSWORD;
-CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY $USER_PASSWORD;
-GRANT ALL PRIVILEGES NO $DB_NAME.* TO '$USER'@'%';
-FLUSH PRIVILEGES;
-EOF
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
-exec mysqld
+    echo "Setupping wordpress database..."
+
+    mariadb -u root << EOF
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
+
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+
+FLUSH PRIVILEGES;
+
+SHUTDOWN;
+
+EOF
+    echo "Stoping mariadb..."
+    sleep 5
+
+else
+    echo "Database already exists, restart service..."
+fi
+
+exec mysqld_safe
--- srcs/requirements/wordpress/tools/wp-builder.sh
+++ srcs/requirements/wordpress/tools/wp-builder.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
-
+echo "Connecting to DB: $DB_NAME as $DB_USER on host $DB_HOST"
 if [ ! -f wp-config.php ]; then
     echo "start building wordpress..."
-    wp core download --allow-root
-    cp wp-config-sample.php wp-config.php
-    # if [ -f wp-config-sample.php ]; then
-    # fi
+    echo "start building wordpress..."
+    wp core download                   \
+        --allow-root
+
+    sleep 3
+
+    wp config create                   \
+        --dbname=$DB_NAME              \
+        --dbuser=$DB_USER              \
+        --dbhost=$DB_HOST              \
+        --dbpass=$DB_USER_PASSWORD     \
+        --allow-root
+    
+    wp core install                    \
+        --url=              $WP_DOMAIN \
+        --title=            $WP_TITLE  \
+        --admin_user=       $WP_ADMIN  \
+        --admin_email=      $WP_EMAIL  \
+        --admin_password=   $WP_PASS   \
+        --skip-email                   \
+        --allow-root
 fi
 
 /usr/sbin/php-fpm8.2 -F
