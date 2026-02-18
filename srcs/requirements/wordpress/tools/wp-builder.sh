a04eedd97fa2d34d5973db73b376082b64ee95af
bn-khalil <benkhaliltouda3201@gmail.com>
2026-02-18 06:37:24 -0500

    feat: compose file to automate process

--- srcs/requirements/wordpress/tools/wp-builder.sh
+++ srcs/requirements/wordpress/tools/wp-builder.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-echo "Connecting to DB: $DB_NAME as $DB_USER on host $DB_HOST"
+
 if [ ! -f wp-config.php ]; then
     echo "start building wordpress..."
     echo "start building wordpress..."
-    wp core download                   \
-        --allow-root
+    wp core download --allow-root
 
     sleep 3
 
-    wp config create                   \
-        --dbname=$DB_NAME              \
-        --dbuser=$DB_USER              \
-        --dbhost=$DB_HOST              \
-        --dbpass=$DB_USER_PASSWORD     \
+    wp config create              \
+        --dbname="$DB_NAME"        \
+        --dbuser="$DB_USER"         \
+        --dbhost="$DB_HOST"          \
+        --dbpass="$DB_USER_PASSWORD"  \
         --allow-root
     
-    wp core install                    \
-        --url=              $WP_DOMAIN \
-        --title=            $WP_TITLE  \
-        --admin_user=       $WP_ADMIN  \
-        --admin_email=      $WP_EMAIL  \
-        --admin_password=   $WP_PASS   \
-        --skip-email                   \
+    wp core install           \
+        --url="$WP_DOMAIN"     \
+        --title="$WP_TITLE"     \
+        --admin_user="$WP_ADMIN" \
+        --admin_email="$WP_EMAIL" \
+        --admin_password="$WP_PASS"\
+        --skip-email                \
         --allow-root
+
+    wp theme install https://github.com/skat-tg/game-of-thrones/archive/refs/heads/master.zip --activate --allow-root
+
+    wp option update blogdescription "Winter is Coming... and wordpress is running." --allow-root
+
+    echo "instalation completed!"
+
 fi
 
 /usr/sbin/php-fpm8.2 -F
